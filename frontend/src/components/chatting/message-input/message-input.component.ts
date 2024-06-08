41ff020dc744edd2c09ddeb0176550c6044a032e
Nava <juannr2002@gmail.com>
2024-06-08 15:54:29 +0200

    Enlace de archivos implementado en el front

--- frontend/src/components/chatting/message-input/message-input.component.ts
+++ frontend/src/components/chatting/message-input/message-input.component.ts
@@ -1,21 +1,43 @@
 import {Component, EventEmitter, Output} from '@angular/core';
 import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {FileAttachmentComponent} from "../../file-attachment/file-attachment.component";
 
 @Component({
   selector: 'app-message-input',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FileAttachmentComponent
   ],
   templateUrl: './message-input.component.html',
   styleUrl: './message-input.component.css'
 })
 export class MessageInputComponent {
-  inputMessage = new FormControl('');
+  messageInput = new FormControl('');
+  filesInput = new FormControl([]);
+  filesToSend?: File[]
 
   @Output() onSendMessage = new EventEmitter<string>();
+  @Output() onSendFiles = new EventEmitter<string>();
 
   triggerMessageSend = (text: string) => {
     this.onSendMessage.emit(text);
+    this.messageInput.setValue('');
+  }
+
+  openFileDialog = (input: HTMLInputElement) => {
+    input.click();
+  }
+
+  triggerFilesSend = (text: string) => {
+    // this.onSendMessage.emit(text);
+    // this.inputMessage.setValue('');
+  }
+
+  handleFiles(event: Event) {
+    const input = event.target as HTMLInputElement;
+    this.filesToSend = Array.from(input.files!);
+
+    console.log(this.filesToSend)
   }
 }
